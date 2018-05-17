4760ee1569bf1a4d8bc82785de14ae2b6ac2aa07
Meeting One <meetingone@Meetings-Mac-mini.local>
2018-05-17 09:44:08 -0500

    Create databse and scaffold vide

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :videos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ root to: 'videos#index'
 end
